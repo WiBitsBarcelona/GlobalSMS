fe7208817f1521804fc7f79d3a38aa638c2fc346
Alejandro Perez <alejandro.globaltank.eu>
2021-07-20 10:54:36 +0200

    PINCODE login

--- app/src/main/java/eu/globaldevelopers/globalsms/LoginActivity.java
+++ app/src/main/java/eu/globaldevelopers/globalsms/LoginActivity.java
@@ -53,7 +53,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "artur@globaltank.eu:Pr0!?2019", "team@globaldevelopers.eu:Sms2020", "99999"
+            "artur@globaltank.eu:Pr0!?2019", "team@globaldevelopers.eu:Sms2020", "031416"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
